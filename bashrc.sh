1f219c8220b9813542974c8aa9d7f908e109c4f1
DylanEHolland <salinson1138@gmail.com>
2019-11-09 08:41:08 -0600

    fixed loading up python3 venv

--- bashrc.sh
+++ bashrc.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
 source /etc/bashrc; # import system settings
-source ~/.environment/bin/activate; # use our local python environment
 
 # Load our dotfiles
 for dot_file in $HOME/.files/{aliases,exports}.sh `ls $HOME/.files/etc/*`; 
@@ -11,3 +10,4 @@ done;
 
 # Load the API keys we don't want being open source (Tiingo, Tenquant, Reddit, etc)
 source $HOME/.api_keys.sh
+source ~/.environment/bin/activate; # use our local python environment
