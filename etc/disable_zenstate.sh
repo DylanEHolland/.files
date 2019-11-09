488c07828501596b3d514ec1b79bf62a49bcc47f
DylanEHolland <salinson1138@gmail.com>
2019-11-09 08:39:57 -0600

    fixed loading up python3 venv

--- etc/disable_zenstate.sh
+++ etc/disable_zenstate.sh
@@ -1,1 +1,7 @@
-sudo python $HOME/bin/zenstates.py --c6-disable;
+#!/usr/bin/env sh
+#
+# Zenstates comes from https://github.com/r4m0n/ZenStates-Linux
+#
+#   Needed to fix annoying freeze on Ryzen + Fedora (maybe all Linux?)
+
+sudo python $HOME/bin/zenstates.py --c6-disable &> /dev/null;
