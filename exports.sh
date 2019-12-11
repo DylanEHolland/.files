5ffc425e568448001cc0430046ffcc83f2379eeb
DylanEHolland <salinson1138@gmail.com>
2019-12-11 14:52:10 -0600

    added watchsync.sh

--- exports.sh
+++ exports.sh
@@ -2,5 +2,8 @@
 
 export LC_ALL="C.utf8";
 export PS1="\[\e[0;32m\][\[\e[0m\]\u@\[\033[01;31m\]\h \[\033[01;34m\]\w\[\e[0;32m\]]\[\e[0m\]$ ";
-export PYTHONPATH="";
+export PYTHONPATH="/home/dylan/src/business/Finance/edgelord/:/home/dylan/src/business/Finance/FinancialModelingPrep/";
+export GOPATH="$HOME/.goenvironment";
 export PATH="$HOME/bin:$PATH";
+export DATASERVER_STOCK_DIRECTORY="/home/dylan/stock_data";
+. ~/.poetry/env;
