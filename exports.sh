3040b97c4d97f50f680e9b4be7cf0edd3bc6000a
dylan <dylan@localhost.localdomain>
2019-12-11 15:55:46 -0600

    updated PATH in imports

--- exports.sh
+++ exports.sh
@@ -4,6 +4,6 @@ export LC_ALL="C.utf8";
 export PS1="\[\e[0;32m\][\[\e[0m\]\u@\[\033[01;31m\]\h \[\033[01;34m\]\w\[\e[0;32m\]]\[\e[0m\]$ ";
 export PYTHONPATH="/home/dylan/src/business/Finance/edgelord/:/home/dylan/src/business/Finance/FinancialModelingPrep/";
 export GOPATH="$HOME/.goenvironment";
-export PATH="$HOME/bin:$PATH";
+export PATH="$HOME/bin:$HOME/.files/bin:$PATH";
 export DATASERVER_STOCK_DIRECTORY="/home/dylan/stock_data";
 . ~/.poetry/env;
