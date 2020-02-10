e5bfa3878ede1e12e1c11e9c67d5eae01adc68ce
DylanEHolland <dh1432A22291>
2020-02-09 18:39:22 -0600

    Rewrote init.sh to install python and javascript + heroku and poetry

--- init.sh
+++ init.sh
@@ -1,1 +1,41 @@
-curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python
+install_javascript() {
+	if ! [ node ]; then
+		sudo dnf install nodejs npm -y;
+	else
+		echo "Node already installed";
+	fi;
+}
+
+install_python() {
+	if ! [ python3 ]; then 
+		sudo dnf install python3 python3-pip;
+	else
+		echo "Python already installed";
+	fi;
+}
+
+install_heroku() {
+	if ! [ -d $HOME/.heroku ]; then
+		OLDEST_DIR=$PWD;
+		cd /tmp &&
+		wget -c https://cli-assets.heroku.com/heroku-linux-x64.tar.xz &&
+		tar xvf heroku-linux-x64.tar.xz &&
+		mv heroku $HOME/.heroku;
+		cd $OLDEST_DIR;
+	else
+		echo "Heroku already installed";
+	fi;
+}
+
+install_poetry() {
+	if ! [ -d $HOME/.poetry ]; then
+		curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python; # Poetry (python packager)
+	else
+		echo "Poetry already installed";
+	fi;
+}
+
+install_javascript;
+install_python;
+install_poetry;
+install_heroku;
