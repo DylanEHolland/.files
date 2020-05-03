1c4a4073db4c014374283823c02f5b3bb558ee92
DylanEHolland <salinson1138@gmail.com>
2020-05-03 04:51:54 -0500

    Updated dnf init script

--- etc/install_centos/bin/dnf-init.sh
+++ etc/install_centos/bin/dnf-init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+RPM_FUSION="https://download1.rpmfusion.org";
+RPM_FUSION_FREE="/free/el/rpmfusion-free-release-7.noarch.rpm";
+RPM_FUSION_NONFREE="/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm";
+
+dnf install $RPM_FUSION$RPM_FUSION_FREE -y;
+dnf install $RPM_FUSION$RPM_FUSION_NONFREE -y;
 dnf update -y;
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
