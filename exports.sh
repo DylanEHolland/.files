9cdfb03fa0fd89150ee8098fb3565fef3954566f
DylanEHolland <salinson1138@gmail.com>
2021-01-22 18:30:06 -0800

    Added drop all tables

--- exports.sh
+++ exports.sh
@@ -11,4 +11,4 @@ export PATH;
 
 export LD_LIBRARY_PATH="/usr/local/cuda-10.2/lib64";
 export PS1="\[\e[0;32m\][\[\e[0m\]\u@\[\033[01;31m\]\h \[\033[01;34m\]\w\[\e[0;32m\]]\[\e[0m\]$ ";
-
+source $HOME/.nix-profile/etc/profile.d/nix.sh
