995d86ccd59a3b266ca99e535fbb7746e3b156d8
DylanEHolland <salinson1138@gmail.com>
2021-04-29 10:18:36 -0700

    Rewrote path in exports.sh

--- exports.sh
+++ exports.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
-export PATH="$HOME/.files/bin:$HOME/.pybuild/bin:/bin:/usr/bin:/sbin:/usr/sbin";
+export PATH="$HOME/.files/bin:$HOME/.pybuild/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/bin";
+export PATH="$HOME/.local/share/JetBrains/Toolbox/apps/PyCharm-C/ch-0/211.7142.13";
 export PS1="\[\e[0;32m\][\[\e[0m\]\u@\[\033[01;31m\]\h \[\033[01;34m\]\w\[\e[0;32m\]]\[\e[0m\]$ ";
