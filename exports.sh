e5a0d5b944fb14c51f112bc8b4d5b67089cc922b
DylanEHolland <salinson1138@gmail.com>
2021-07-02 13:38:50 -0700

    Added python to path

--- exports.sh
+++ exports.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 export PATH="$HOME/.files/bin:$HOME/.pybuild/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/bin";
-export PATH="$HOME/.local/share/JetBrains/Toolbox/apps/PyCharm-C/ch-0/211.7142.13";
+export PATH="$PATH:$HOME/.local/share/JetBrains/Toolbox/apps/PyCharm-C/ch-0/211.7142.13/bin";
+export PATH="$PATH:/usr/local/lib/node_modules/*/bin";
 export PS1="\[\e[0;32m\][\[\e[0m\]\u@\[\033[01;31m\]\h \[\033[01;34m\]\w\[\e[0;32m\]]\[\e[0m\]$ ";
