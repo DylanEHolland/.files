05a9a767e8a1212f572c966223c8a5d1e807ed58
DylanEHolland <salinson1138@gmail.com>
2021-08-08 12:46:05 -0700

    Added condition to create ~/bin, added ~/.build directories to path

--- bashrc.sh
+++ bashrc.sh
@@ -5,16 +5,8 @@ if [ -f "/etc/bashrc" ]; then
 fi;
 source /etc/profile.d/bash_completion.sh;
 
-ZS_FILE_URL="https://raw.githubusercontent.com/r4m0n/ZenStates-Linux/master/zenstates.py";
-if ! [ -f "$HOME/bin/zenstates.py" ]; then
-	if ! [ -d "$HOME/bin" ]; then
-		mkdir "$HOME/bin";
-	fi;
-	cd $HOME/bin;
-	wget -c "$ZS_FILE_URL";
-	chmod +x zenstates.py;
-	cd -;
-A
+if ! [ -d "$HOME/bin" ]; then
+	mkdir "$HOME/bin";
 fi;
 
 # Load our dotfiles
--- exports.sh
+++ exports.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-export PATH="$HOME/.files/bin:$HOME/.pybuild/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/bin";
-export PATH="$PATH:$HOME/.local/share/JetBrains/Toolbox/apps/PyCharm-C/ch-0/211.7142.13/bin";
+export PATH="$HOME/.files/bin:$HOME/.build/builder/bin:$HOME/.build/packages/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/bin:$HOME/bin";
+export PATH="$PATH:$HOME/.local/share/JetBrains/Toolbox/apps/PyCharm-C/ch-0/211.7628.24/bin";
 export PATH="$PATH:/usr/local/lib/node_modules/*/bin";
+export PATH="/usr/local/cuda-10.0/bin:/usr/local/cuda/bin:/home/dylan/.local/bin:$PATH:/snap/bin";
+export LD_LIBRARY_PATH="/usr/local/cuda/lib64:/usr/local/cuda-10.0/lib64"
 export PS1="\[\e[0;32m\][\[\e[0m\]\u@\[\033[01;31m\]\h \[\033[01;34m\]\w\[\e[0;32m\]]\[\e[0m\]$ ";
