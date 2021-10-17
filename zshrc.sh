42fe839f48d3d260c979e9a727601feeb499c9be
DylanEHolland <salinson1138@gmail.com>
2021-10-17 14:40:36 -0700

    Changed zsh theme, added snip to fix git autocompletion slow down

--- zshrc.sh
+++ zshrc.sh
@@ -8,7 +8,7 @@ export ZSH="/home/dylan/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+ZSH_THEME="clean"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -101,3 +101,7 @@ source $ZSH/oh-my-zsh.sh
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
 unsetopt share_history;
+
+__git_files () { 
+    _wanted files expl 'local files' _files     
+}
